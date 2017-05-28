d51773799380d5c4bddf621533a07497594d18a4
feiliu <feiliu@mogujie.com>
2017-05-28 22:57:23 +0800

    test

--- src/main/java/com/richard/App.java
+++ src/main/java/com/richard/App.java
@@ -1,13 +1,30 @@
 package com.richard;
 
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws Exception {
+        Vertx vertx = Vertx.vertx();
+
+        HttpServer server = vertx.createHttpServer();
+
+        server.requestHandler(request -> {
+
+            // This handler gets called for each request that arrives on the server
+            HttpServerResponse response = request.response();
+            response.putHeader("content-type", "text/plain");
+
+            // Write to the response and end it
+            response.end("Hello World!");
+        });
+
+        server.listen(8080);
+
+        System.out.println("Hello World!");
     }
 }
